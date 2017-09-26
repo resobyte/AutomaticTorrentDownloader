fd9b1bb0509da4f9745741e49074198d024de8f8
İsmail Reşat Akcan <iresatakcan.bm@gmail.com>
2017-09-26 03:11:10 +0300

    Add Timer on working CMD
    Saat 08' de kapatılması cmd komut istemi ile ayarlandı.

--- uTorrent/Form1.cs
+++ uTorrent/Form1.cs
@@ -31,29 +31,43 @@ namespace uTorrent
             OpenFileDialog file = new OpenFileDialog();
             file.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
             file.Filter = "Torrentler |*.torrent";
-            file.ShowDialog();
+    
             string DosyaYolu = "";
             string DosyaAdi = " ";
+
             if (file.ShowDialog() == DialogResult.OK)
             {
                 DosyaYolu = file.FileName;
                 DosyaAdi = file.SafeFileName;
-                    
+
             }
-           
-                ISimpleTrigger trigger = (ISimpleTrigger)TriggerBuilder.Create()
-                    .WithIdentity("trigger1", "group1") // Trigger name ve group
-                    .WithSimpleSchedule(x => x
-                        .WithIntervalInMinutes(1) // Ne kadar süre aralığında çalığacağı belirleniyor - 1 dakika
-                        .RepeatForever()) // Sonsuza Dek
-                    .EndAt(DateBuilder.DateOf(22, 0, 0)) // Belirlenen saat: 22:00
-                    .Build();
-               
+
+            ////ISimpleTrigger trigger = (ISimpleTrigger)TriggerBuilder.Create()
+            ////    .WithIdentity("trigger1", "group1") // Trigger name ve group
+            ////    .WithSimpleSchedule(x => x
+            ////        .WithIntervalInMinutes(1) // Ne kadar süre aralığında çalığacağı belirleniyor - 1 dakika
+            ////        .RepeatForever()) // Sonsuza Dek
+            ////    .EndAt(DateBuilder.DateOf(22, 0, 0)) // Belirlenen saat: 22:00
+            ////    .Build();
+            /// 
+
+            Process.Start(Path.Combine(DosyaYolu));
+            Process process = new Process();
+            process.StartInfo.FileName = "cmd.exe";
+            DateTime dt = new DateTime();
+            dt = DateTime.Now;
+            DateTime dt1 = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 8, 0,0);
+
+            TimeSpan dtTotal = (dt1 - dt);
+            double total = (dtTotal.Hours * 3600 + dtTotal.Minutes * 60 + dtTotal.Seconds);
+
+            process.StartInfo.Arguments = "/K shutdown -s -t "+total ;
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Maximized;
+            process.Start();
+            Thread.Sleep(2000);
+            process.Kill();
 
             
-            
-
-
         }
     }
 }
